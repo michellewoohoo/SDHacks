18d1072d3b2b56480bf772dc720a2131d6d15aed
christyono <wchristy@uci.edu>
2017-10-21 06:28:06 -0700

    Add files via upload

--- Main.js
+++ Main.js
@@ -1,15 +1,12 @@
-const http = require('http');
-const fs = require('fs');
-const hostname = '127.0.0.1';
-const port = 3000;
+const express = require('express')
+const app = express()
 
-const server = http.createServer(function(req, res){
-	console.log('request was made: ' + req.url);
-	if(req.url === '/home' || req.url === '/'){
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		fs.createReadStream(__dirname + 'home.html').pipe(res);
-	}
-});
+app.use(express.static('public'));
 
-server.listen(port, hostname);
-console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/home.html');
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
