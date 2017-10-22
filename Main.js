032ba72dd87e11e93d49844aa275b17fe6acd028
peteryesh <Noh@Peters-MacBook-Pro.local>
2017-10-21 18:09:10 -0700

    more commitment

--- Main.js
+++ Main.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'));
+app.use(express.static('templates'));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/home.html');
+  res.sendFile(__dirname + '/templates/home.html');
 })
 
-app.listen(3000, function () {
+app.listen(5000, function () {
   console.log('Example app listening on port 3000!')
 })
